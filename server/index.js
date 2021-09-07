b63c6f8163eeacae04908e317dcd6f812ee751b8
Matthew Ty <matthewty1220@gmail.com>
2021-09-07 12:18:59 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../index.html"));
+});
+
+app.get("/css", function (req, res) {
+    res.sendFile(path.join(__dirname, "../index.css"));
+});
+
+const port = process.env.PORT || 4545;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
