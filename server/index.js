b28a9b627c13205b7cbd5461bea11fe13c909f40
Matthew Ty <matthewty1220@gmail.com>
2021-09-07 14:41:32 -0600

    added css functionality

--- server/index.js
+++ server/index.js
@@ -15,6 +15,30 @@ app.get("/css", function (req, res) {
     res.sendFile(path.join(__dirname, "../index.css"));
 });
 
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../index2.html"));
+});
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../index3.html"));
+});
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../index4.html"));
+});
+
+app.get("/css", function (req, res) {
+    res.sendFile(path.join(__dirname, "../index2.css"));
+});
+
+app.get("/css", function (req, res) {
+    res.sendFile(path.join(__dirname, "../index3.css"));
+});
+
+app.get("/css", function (req, res) {
+    res.sendFile(path.join(__dirname, "../index4.css"));
+});
+
 const port = process.env.PORT || 4545;
 
 app.listen(port, () => {
